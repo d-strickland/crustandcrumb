5cd6de950e9d4b1d1e86b1e46803c15f91111a54
David Strickland <dstrickland@austin.utexas.edu>
2017-03-25 21:42:55 -0500

    Create sourdough calculator.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert('Hello, world!');
+function updateRecipe() {
+    let flour_wt = document.getElementById('new_flour_wt').value;
+    let water_pct = document.getElementById('water_pct').value;
+    let starter_pct = document.getElementById('starter_pct').value;
+    let salt_pct = document.getElementById('salt_pct').value;
+    let yeast_pct = document.getElementById('yeast_pct').value;
+
+    document.getElementById('flour_wt').innerHTML = flour_wt + ' oz';
+    document.getElementById('water_wt').innerHTML = (flour_wt * water_pct / 100) + ' oz';
+    document.getElementById('starter_wt').innerHTML = (flour_wt * starter_pct / 100) + ' oz';
+    document.getElementById('salt_wt').innerHTML = (flour_wt * salt_pct / 100) + ' oz';
+    document.getElementById('yeast_wt').innerHTML = (flour_wt * yeast_pct / 100) + ' oz';
+}
+
