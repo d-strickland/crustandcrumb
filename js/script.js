30dd28a394d093088c7cff13c4823447f79d871a
David Strickland <dstrickland@athenahealth.com>
2018-08-16 23:11:41 -0500

    Make everything a bit more user-friendly.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@ function updateRecipe() {
     let water_pct = document.getElementById('water_pct').value;
     let starter_pct = document.getElementById('starter_pct').value;
     let salt_pct = document.getElementById('salt_pct').value;
-    let yeast_pct = document.getElementById('yeast_pct').value;
 
     document.getElementById('flour_wt').innerHTML = flour_wt + ' oz';
     document.getElementById('water_wt').innerHTML = (flour_wt * water_pct / 100) + ' oz';
     document.getElementById('starter_wt').innerHTML = (flour_wt * starter_pct / 100) + ' oz';
     document.getElementById('salt_wt').innerHTML = (flour_wt * salt_pct / 100) + ' oz';
-    document.getElementById('yeast_wt').innerHTML = (flour_wt * yeast_pct / 100) + ' oz';
 }
 
